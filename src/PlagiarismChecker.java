a7e7c6869a39ff66167863e2b842d14fcedf0c7b
suryadedatta <surya.dedatta@menloschool.org>
2026-02-09 22:21:28 -0800

    Continued V1

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -20,14 +20,38 @@ public class PlagiarismChecker {
         int n1 = doc1.length();
         int n2 = doc2.length();
         int[][] array = new int[n1 + 1][n2 + 1];
-        return recursiveFunc(array, (int) (0.5 * n1), (int) (0.5*n2));
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 0; j < array[0].length; j++)
+            {
+                array[i][j] = -1;
+            }
+
+        }
+        return recursiveFunc(array, n1 - 1, n2 - 1, doc1, doc2) + 1;
         // Fill with 0s at index 0 for each
     }
-    public static int recursiveFunc(int[][] array, int index, int index2)
+    public static int recursiveFunc(int[][] array, int index, int index2, String doc1, String doc2)
     {
+        int result;
+        if(array[index][index2] != -1)
+        {
+            return array[index][index2];
+        }
         if(index == 0 || index2 == 0)
         {
             return 0;
         }
+        if(doc1.charAt(index) == doc2.charAt(index2))
+        {
+            // change to -
+            result = 1 + recursiveFunc(array, index - 1, index2 - 1, doc1, doc2);
+        }
+        else
+        {
+            result = Math.max(recursiveFunc(array, index - 1, index2, doc1, doc2), recursiveFunc(array, index, index2 - 1, doc1, doc2));
+        }
+        array[index][index2] = result;
+        return result;
     }
 }
